5bd835b89f6ac713c6d2a41fc6a104c77cd6a7aa
unknown <ali.maameri@univ-constantine2.dz>
2023-11-26 18:57:45 +0100

    settings cros updated

--- Schoolproject/settings.py
+++ Schoolproject/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = ['*']
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
     "https://aml-school.com",
+    
 ]
 # CORS_ALLOW_ORIGIN_WHITELIST = [
 #     "http://localhost:5173",  
