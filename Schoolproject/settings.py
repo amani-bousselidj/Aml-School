2449fd6e3c201f3127b9a49b6a9151d115dc8739
ali-maameri02 <ali.maameri@univ-constantine2.dz>
2023-12-09 13:44:25 +0100

    permission settinjs off

--- Schoolproject/settings.py
+++ Schoolproject/settings.py
@@ -185,9 +185,9 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Additional static file locations (if needed)
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'Schoolproject/static')
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, 'static')
+# ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
