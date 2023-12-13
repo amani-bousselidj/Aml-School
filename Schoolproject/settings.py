c708c1fd2d50d592700661103c3fb1cd42fe0796
ali-maameri02 <ali.maameri@univ-constantine2.dz>
2023-12-13 02:34:39 +0100

    test facebook login

--- Schoolproject/settings.py
+++ Schoolproject/settings.py
@@ -75,7 +75,7 @@ MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.locale.LocaleMiddleware',
-
+    'allauth.account.middleware.AccountMiddleware',
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
